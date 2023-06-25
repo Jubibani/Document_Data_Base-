e271a7c13c386c059952ffe34970b8f401cb47f7
Jubibani <125711731+Jubibani@users.noreply.github.com>
2023-06-25 01:10:16 -0700

    change plan.create

--- main.py
+++ main.py
@@ -1,41 +1,54 @@
 # by: Christopher Jiovanni A. Orpilla
-# # creating an account
-# import pymongo
+# create an application
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.widget import Widget
+class BoxLayoutExample(BoxLayout):
 
-def create():
-    print("-- create an Account --")
-    user_name = input("Enter username: ")
-    password = input("password: ")
-    print("one line added")
-    print(" --- Account created ---  ")
-    print("Hello " + user_name + "!")
-    print(" [Please don't share your password] ")
-    print("password: " + password)
-    # verifying the account
-    print("-- -log in your account - -- ")
-    def log_in():
-        print("-------------------------------")
-        print("Welcome Back " + user_name + "!")
-        print()
-        print("[q] to quit")
-        print("[+] to add")
-        print("[-] to delete")
-        print("--------------------------------")
-        search = input("Search or type a keyword: ")
-        print("--------------------------------")
-        print()
-        print("--------------------------------")
-        print("you searched " + "'" + search + "'")
-    while True:
-        enter_password = input("Enter password: ")
-        if enter_password == password:
-            log_in()
-            break
-        elif enter_password == 'q':
-            create()
-        else:
-            print("try again")
+    pass
 
-    data()
+class MainWidget(Widget):
 
-create()
+    pass
+class ThelabApp(App):
+    pass
+
+ThelabApp().run()
+
+# def create():
+#     print("-- create an Account --")
+#     user_name = input("Enter username: ")
+#     password = input("password: ")
+#     print("one line added")
+#     print(" --- Account created ---  ")
+#     print("Hello " + user_name + "!")
+#     print(" [Please don't share your password] ")
+#     print("password: " + password)
+#     # verifying the account
+#     print("-- -log in your account - -- ")
+#     def log_in():
+#         print("-------------------------------")
+#         print("Welcome Back " + user_name + "!")
+#         print()
+#         print("[q] to quit")
+#         print("[+] to add")
+#         print("[-] to delete")
+#         print("--------------------------------")
+#         search = input("Search or type a keyword: ")
+#         print("--------------------------------")
+#         print()
+#         print("--------------------------------")
+#         print("you searched " + "'" + search + "'")
+#     while True:
+#         enter_password = input("Enter password: ")
+#         if enter_password == password:
+#             log_in()
+#             break
+#         elif enter_password == 'q':
+#             create()
+#         else:
+#             print("try again")
+#
+#     data()
+#
+# create()
